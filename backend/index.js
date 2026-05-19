01731034a5d82e501f44c9f7a5d20e2fd4059315
aryan <gargaryan12122005@gmail.com>
2026-05-19 18:18:21 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: 'https://carvereels-v2-5.onrender.com',
     }
 ));
 
